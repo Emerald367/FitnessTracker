ad2f7e65ed83b884a21f9984b33a4157bab7cf0f
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-05-26 11:53:54 -0700

    Setup React Router

--- fitness-tracker/src/App.js
+++ fitness-tracker/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import WorkoutPlanForm from './components/WorkoutPlanForm';
+import ManageWorkoutPlans from './components/ManageWorkoutPlans';
+import WorkoutHistory from './components/WorkoutHistory';
 import './App.css';
-import WorkoutPlanForm from './components/WorkoutPlanForm'
 import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <WorkoutPlanForm/>
+    <Router>
+      <div className="flex min-h-screen bg-gray-50">
+        {/* Sidebar */}
+        <div className="w-1/4 bg-indigo-600 text-white p-6">
+          <h2 className="text-2xl font-bold mb-4">Contents</h2>
+          <ul>
+            <li className="mb-2">
+              <Link className="text-indigo-200 hover:text-white" to="/create-workout">
+                Create New Workout Plan
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link className="text-indigo-200 hover:text-white" to="/manage-workouts">
+                Manage Current Workout Plans
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link className="text-indigo-200 hover:text-white" to="/workout-history">
+                Workout History
+              </Link>
+            </li>
+          </ul>
+        </div>
+      
+
+    {/* Main Content */}
+    <div className="w-3/4 p-6">
+      <Routes>
+        <Route path="/create-workout" element={<WorkoutPlanForm/>} />
+        <Route path="/manage-workouts" component={<ManageWorkoutPlans />} />
+        <Route path="/workout-history" component={<WorkoutHistory />} />
+      </Routes>
     </div>
+  </div>
+    </Router>
   );
 }
 
