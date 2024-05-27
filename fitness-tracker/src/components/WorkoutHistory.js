c478bfbba15d2412cdfda8a62e40c8ecfb13c86a
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-05-27 14:06:17 -0700

    Workout History component created

---
+++ fitness-tracker/src/components/WorkoutHistory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WorkoutHistory = () => {
+    return (
+        <div>
+            <h1>Workout History</h1>
+        </div>
+    );
+};
+
+export default WorkoutHistory;
