34b99ed1ad08dc9a2e7dbb17ebb65cb0d9fc39f3
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-06-11 08:35:02 -0700

    Functionality and design for workout history

--- fitness-tracker/src/components/WorkoutHistory.js
+++ fitness-tracker/src/components/WorkoutHistory.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { format, parseISO } from 'date-fns';
+
 
 const WorkoutHistory = () => {
+  const [workoutHistory, setWorkoutHistory] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchWorkoutHistory = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get('http://localhost:5000/workout-history');
+        console.log('API Response:', response.data);
+        const filteredData = response.data.filter(history => history.workoutdate !== null)
+        setWorkoutHistory(filteredData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching workout history:', error);
+        setError('Failed to load workout history');
+        setLoading(false);
+      }
+    };
+
+    fetchWorkoutHistory();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
     return (
-        <div>
-            <h1>Workout History</h1>
-        </div>
+      <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4">
+        <strong className="font-bold">Error: </strong>
+        <span className="block sm:inline">{error}</span>
+      </div>
     );
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100 p-6">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold text-indigo-700 mb-6 text-center">Workout History</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {workoutHistory.map((history, index) => (
+            <div key={index} className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+              <div className="mb-4">
+                <h3 className="text-xl font-semibold text-indigo-700">{history.completedplanname}</h3>
+              </div>
+              <div className="flex items-center text-gray-700">
+                <svg className="w-6 h-6 mr-2 text-indigo-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10m-4 5h3m-3 4h3m-6-8h3m-3 4h3m-3 4h3"></path></svg>
+                <span>{format(parseISO(history.workoutdate), 'MMMM d, yyyy')}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
+    
 
 export default WorkoutHistory;
